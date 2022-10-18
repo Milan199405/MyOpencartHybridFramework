dc86f1e7d17e354bac3221ae6485c245ab4c3bf5
Mrutyunjaya Samantray <s.mrutyunjaya94@gmail.com>
2022-10-18 10:21:32 +0530

    sample changes 4

--- src/main/java/com/qa/opencart/utils/Constants.java
+++ src/main/java/com/qa/opencart/utils/Constants.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 
 public class Constants {
 	public static final String LOGIN_PAGE_URL = "https://naveenautomationlabs.com/opencart/index.php?route=account/login";
-	public static final String LOGIN_PAGE_TITLE = "Account Loginn";
+	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	public static final String ACCOUNT_PAGE_TITLE = "My Account";
 
-	public static final int DEFAULT_TIMEOUT = 10;
+	public static final int DEFAULT_TIMEOUT = 5;
 
 	public static final String LOGIN_PAGE_ERROR_MSG = "Warning: No match for E-Mail Address and/or Password.";
 	public static final String ACCOUNT_CREATION_SUCCESS_MSG = "Your Account Has Been Created!";
