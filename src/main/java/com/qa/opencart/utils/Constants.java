49a54e0c3d07fcf8201bf3645a94e671b2fa2095
Milan199405 <68681280+Milan199405@users.noreply.github.com>
2022-10-18 10:04:33 +0530

    Update Constants.java

--- src/main/java/com/qa/opencart/utils/Constants.java
+++ src/main/java/com/qa/opencart/utils/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
 	public static final String LOGIN_PAGE_URL = "https://naveenautomationlabs.com/opencart/index.php?route=account/login";
 	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	public static final String ACCOUNT_PAGE_TITLE = "My Account";
-	public static final int DEFAULT_TIMEOUT = 5;
+	public static final int DEFAULT_TIMEOUT = 7;
 	public static final String LOGIN_PAGE_ERROR_MSG = "Warning: No match for E-Mail Address and/or Password.";
 	public static final String ACCOUNT_CREATION_SUCCESS_MSG = "Your Account Has Been Created!";
 	public static final String REGISTER_SHEET_NAME = "Registration";
