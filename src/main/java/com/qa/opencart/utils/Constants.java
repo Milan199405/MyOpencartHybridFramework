9d8858e3438d159d65ee2c8b3336be096dbf9a56
Mrutyunjaya Samantray <s.mrutyunjaya94@gmail.com>
2022-10-18 13:41:25 +0530

    normal changes

--- src/main/java/com/qa/opencart/utils/Constants.java
+++ src/main/java/com/qa/opencart/utils/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
 	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	public static final String ACCOUNT_PAGE_TITLE = "My Account";
 
-	public static final int DEFAULT_TIMEOUT = 10;
+	public static final int DEFAULT_TIMEOUT = 7;
 
 	public static final String LOGIN_PAGE_ERROR_MSG = "Warning: No match for E-Mail Address and/or Password.";
 	public static final String ACCOUNT_CREATION_SUCCESS_MSG = "Your Account Has Been Created!";
